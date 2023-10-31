e6086747879eddb7241c507b742360f3b6ea2b7f
yahianiismail <Hawma@122020>
2023-10-31 22:56:50 +0100

    saves error

--- src/app/services/authent.service.ts
+++ src/app/services/authent.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs';
-import {User} from '../model/user';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthentService {
   private httpOption = new HttpParams();
   constructor(private http: HttpClient) {
     this.httpOption.append('Content-Type', 'application/json');
-    this.httpOption.append('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    this.httpOption.append('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    this.httpOption.append(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    this.httpOption.append(
+      'Access-Control-Allow-Methods',
+      'POST, GET, PUT, DELETE, OPTIONS',
+    );
   }
-  public createUser(api: string , user: User): Observable<any>{
+  public createUser(api: string, user: User): Observable<any> {
     return this.http.post(api, user);
   }
-  // tslint:disable-next-line:typedef
-  public  login(api: string, user: User){
+  public login(api: string, user: User) {
     // @ts-ignore
     return this.http.get(api, this.httpOption, user);
   }
